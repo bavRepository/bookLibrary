c685add30f201a6c5a3427d70f4f6863f739db97
Anton <a.barai6003@gmail.com>
2025-01-23 11:35:26 +0200

    InitialCommitAfterProjectAdding

---
+++ frontend/src/components/BookForm/BookForm.js
@@ -0,0 +1,11 @@
+import './bookForm.scss'
+
+const BookForm = () => {
+  return (
+    <div className="app-block book-form">
+      <h2>Add new Book</h2>
+    </div>
+  )
+}
+
+export default BookForm
---
+++ frontend/src/components/BookList/BookList.js
@@ -0,0 +1,11 @@
+import './bookList.scss'
+
+const BookList = () => {
+  return (
+    <div className="app-block book-list">
+      <h2>Book list</h2>
+    </div>
+  )
+}
+
+export default BookList
