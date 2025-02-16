71684792f546640b00f9256d1630444ff4dd49c8
Anton <a.barai6003@gmail.com>
2025-02-16 11:48:56 +0200

    reinstallNpmPackages

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,30 +1,31 @@
-import './bookList.scss'
-import { useSelector } from 'react-redux'
+import "./bookList.scss"
+import { useSelector } from "react-redux"
 
 const BookList = () => {
-  const books = useSelector((state) => state.books)
+	const books = useSelector(state => state.books)
 
-  return (
-    <div className="app-block book-list">
-      <h2>Book list</h2>
+	return (
+		<div className='app-block book-list'>
+			<h2>Book list</h2>
 
-      {books.length === 0 ? (
-        <p>No books available</p>
-      ) : (
-        <ul>
-          {books.map((book, i) => {
-            return (
-              <li key={book.id}>
-                <div className="book-info">
-                  {++i}. {book.title} by <strong>{book.author}</strong>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
-      )}
-    </div>
-  )
+			{books.length === 0 ? (
+				<p>No books available</p>
+			) : (
+				<ul>
+					{books.map((book, i) => {
+						return (
+							<li key={book.id}>
+								<div className='book-info'>
+									{++i}. {book.title} by <strong>{book.author}</strong>
+								</div>
+								<button>clear</button>
+							</li>
+						)
+					})}
+				</ul>
+			)}
+		</div>
+	)
 }
 
 export default BookList
