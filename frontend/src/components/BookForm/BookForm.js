134a0cab1cd7fe61ab97449179fbe043115d5ae3
Anton <a.barai6003@gmail.com>
2025-06-06 09:46:00 +0300

    Dispatch error in the async thunk

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -27,7 +27,7 @@ const BookForm = () => {
 	}
 
 	const handleAddRandomBookViaAPI = () => {
-		dispatch(fetchBook())
+		dispatch(fetchBook(`http://localhost:4000/random-book`))
 	}
 
 	const handleAddRandomBook = async () => {
